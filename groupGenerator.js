1e46086870841483b5520d4c627f695f79b25c86
mihkel.liblikman@gmail.com <mihkel.liblikman@gmail.com>
2020-01-08 02:46:40 +0200

    more minor fixes

--- groupGenerator.js
+++ groupGenerator.js
@@ -3,19 +3,20 @@ function mapArray() {
     let namespergroup = document.getElementById('perGroup').value;
     let	allnameslen = allnames.length;
     let grouping = document.querySelector('.groups');
-    let	numgroups = Math.ceil(allnameslen / namespergroup);
     j = 1;
 
-    if ( numgroups !== "undefined") {
-        numgroups = document.getElementById('numGroups').value;
-        namespergroup = allnameslen / numgroups;
+    if ( numGroups !== "undefined") {
+        numGroups = document.getElementById('numGroups').value;
+        namespergroup = allnameslen / numGroups;
     }
     
     while (grouping.firstChild) {
         grouping.removeChild(grouping.firstChild);
     }
     
-    for (i = 0; i < numgroups; i++) {
+    grouping.innerHTML = `<h1 class="team-title">Here are the <span class='hl'>Teams</span></h1>`
+
+    for (i = 0; i < numGroups; i++) {
         let first = document.createElement('div');
         first.innerHTML = `<div class="group col-md-4"><h2 class="group-header">Group ${j++}</h2></div>`;
         grouping.appendChild(first);
